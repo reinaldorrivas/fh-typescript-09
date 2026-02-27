cfb442c0611f2acb5e4728f7e71699d2d4564d5e
René Rivas <reinaldorrivas@gmail.com>
2026-02-26 21:01:53 -0400

    fh-ts-guide: Minor change

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -1,6 +1,6 @@
-export const genericsIntro = (): void => {
-  const genericFn = <T>(argument: T): T => argument;
+import { genericFn } from "../utils/global";
 
+export const genericsIntro = (): void => {
   console.log(genericFn(3.1416).toFixed(2));
   console.log(genericFn("Lalo").toUpperCase());
   console.log(genericFn(new Date().getDate()));
